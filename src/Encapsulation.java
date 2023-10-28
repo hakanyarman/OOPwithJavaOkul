75f74e3c1076b06e7aba9f0bb9f559afbd81ba24
hakanyarman <104122864+hakanyarman@users.noreply.github.com>
2023-10-28 14:39:56 +0300

    add comment lines

--- src/Encapsulation.java
+++ src/Encapsulation.java
@@ -6,9 +6,15 @@ public class Encapsulation {
         this.username = username;
     }
 
-    public String getPassword(){
-        return password;
-    }
+
+    // public String getPassword(){
+    //     return password;
+    // }
+    // now password is write-only. it's value can not getted.
+
+    // read-only => if there is only get method and not set method
+    // write-only => if there is only set method and not get method
+
     public void setPassword(String newPassword){
         this.password = newPassword;
     }
